25f3e2cdb979659cb33c32610c23259320d69a48
Kantipudi Satya Gopika <kantipudikantipudi1432@gmail.com>
2026-02-12 23:43:29 +0530

    Create problem 169.py

---
+++ problem 169.py
@@ -0,0 +1,9 @@
+//Majority Element //
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq=dict()
+        for num in nums:
+            if num in freq:freq[num]+=1
+            else:freq[num]=1
+            if freq[num]>len(nums)//2:return num
