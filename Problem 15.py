e8402ab9333c3837672651c0b5763f0180148035
Kantipudi Satya Gopika <kantipudikantipudi1432@gmail.com>
2026-02-12 23:24:31 +0530

    Create Problem 15.py

---
+++ Problem 15.py
@@ -0,0 +1,25 @@
+//3sum//
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result=[]
+        j,k=0,0
+        for i in range(len(nums)):
+            if i>0 and nums[i-1]==nums[i]:
+                continue
+            j,k=i+1,len(nums)-1
+            while j<k:
+                if nums[i]+nums[j]+nums[k]==0:
+                    result.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k+1]:
+                        k-=1
+                elif nums[i]+nums[j]+nums[k]>0:
+                    k-=1
+                elif nums[i]+nums[j]+nums[k]<0:
+                    j+=1
+        return result
