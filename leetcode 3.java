7664a1bbad75e5465ea647988e79c806bb65d663
Kantipudi Satya Gopika <kantipudikantipudi1432@gmail.com>
2026-02-20 21:40:47 +0530

    Create leetcode 3.java

---
+++ leetcode 3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int i=0,j=0,max=0;
+        HashSet<Character> set = new HashSet<>();
+
+        while(i < s.length()){
+            if(!set.contains(s.charAt(i))){
+                set.add(s.charAt(i));
+                i++;
+                max = Math.max(max, set.size());
+            }else{
+                set.remove(s.charAt(j));
+                j++;
+            }
+        }
+        return max;
+    }
+}
