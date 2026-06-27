41499a83a825816875245e00d84a9d389f294c06
Kantipudi Satya Gopika <kantipudikantipudi1432@gmail.com>
2026-06-27 22:34:28 +0530

    Create leetcode18.cpp

---
+++ leetcode18.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        if (n < 4) return result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; ++i) {
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; ++j) {
+                // Skip duplicates for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    // Use long long to prevent integer overflow during summation
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        
+                        // Skip duplicates for third and fourth numbers
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
